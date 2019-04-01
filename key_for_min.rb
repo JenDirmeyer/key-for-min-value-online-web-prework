cc953f5831045b32186eefe2e34a45731aaad659
Jennifer Dirmeyer <dirmeyer@gmail.com>
2019-04-01 01:41:23 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -5,6 +5,7 @@ def key_for_min_value(name_hash)
       min_value = nil
     name_hash.each do |key, value| 
       if min_value == nil || value < min_value
+        min_value = value
         
       end  
     end
